c9e13208acf87a792c3f8a346050355985ef58f4
RameshPlato <ramesh@platoonline.com>
2025-04-09 09:57:29 +0530

    Dark them

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -7,7 +7,7 @@ import {
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, ModalController } from '@ionic/angular';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { OtherService } from '../service/other/other.service';
 import { ServerService } from '../service/server.service';
 @Component({
@@ -22,14 +22,20 @@ export class TabsPage implements OnInit {
   isMobile: boolean = false;
   dir = 'ltr';
   data:any;
+  user_id:any;
 
   constructor(
       public otherService: OtherService,
          public server: ServerService,
-  ) {}
+         private router:Router
+  ) {
+    this.user_id = localStorage.getItem('user_id');
+
+  }
 
   ngOnInit() {
     this.otherService.triggerLoadData.subscribe(() => {
+      this.user_id = localStorage.getItem('user_id');
       this.loadData(); 
     });
     this.loadData();
@@ -43,9 +49,22 @@ export class TabsPage implements OnInit {
   private checkScreenSize() {
     this.isMobile = window.innerWidth <= 768; // Adjust the breakpoint as needed
   }
+
+  async getAccount(){
+    if(this.user_id){
+      this.router.navigate(['/tabs/account']);
+
+    }else{
+      this.router.navigate(['/login']);
+    }
+
+  }
+
   async loadData() {
     this.server.homepage().subscribe((response: any) => {
       this.data = response.data;
     });
   }
+
+
 }
